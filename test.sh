a0b48c3d34609b25fd16eba77dde0863a0dff13f
Gilad Barnea <giladbrn@gmail.com>
2025-08-26 09:24:58 +0200

    test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "hi"
+curl -i https://typingmind-plugin-server.vercel.app/hi ;
+
+echo "first read"
+curl -i https://typingmind-plugin-server.vercel.app/pad/sanity-123 ; 
+
+echo "write"
+curl -i -X POST https://typingmind-plugin-server.vercel.app/pad/sanity-123 -H 'content-type: application/json' -d '{"text":"hello from curl"}' ;
+
+echo "read again"
+curl -i https://typingmind-plugin-server.vercel.app/pad/sanity-123 ;
+
