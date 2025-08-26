c2c1ad903b3aa1e9a59ec56ddaf54e991184e951
Gilad Barnea <giladbrn@gmail.com>
2025-08-26 17:49:49 +0200

    improved test.sh

--- test.sh
+++ test.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 set -e
 
-echo "hi"
+rand=$(LC_ALL=C tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 4)
+echo " ✦ Using random string: $rand"
+
+echo " ✦ GET /hi..."
 response1=$(curl -s --fail https://typingmind-plugin-server.vercel.app/hi)
-echo "$response1"
+printf " ✦ → %s " "$response1"
 [[ "$response1" = '{"ok":true}' ]]
+printf "✔\n"
 
-echo "first read"
-response2=$(curl -s --fail https://typingmind-plugin-server.vercel.app/pad/sanity-123)
-echo "$response2"
+echo " ✦ First GET /pad/sanity-$rand..."
+response2=$(curl -s --fail https://typingmind-plugin-server.vercel.app/pad/sanity-$rand)
+printf " ✦ → %s " "$response2"
 [[ "$response2" = '{"text":""}' ]]
+printf "✔\n"
 
-# Generate a short random string
-rand=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8)
-echo "Using random string: $rand"
 
-echo "write"
-response3=$(curl -s --fail -X POST https://typingmind-plugin-server.vercel.app/pad/sanity-123 -H 'content-type: application/json' -d "{\"text\":\"hello from curl $rand\"}")
-echo "$response3"
+echo " ✦ POST /pad/sanity-$rand..."
+response3=$(curl -s --fail -X POST https://typingmind-plugin-server.vercel.app/pad/sanity-$rand -H 'content-type: application/json' -d "{\"text\":\"hello from curl $rand\"}")
+printf " ✦ → %s " "$response3"
 [[ "$response3" = "{\"ok\":true,\"text\":\"hello from curl $rand\"}" ]]
+printf "✔\n"
 
-echo "read again"
-response4=$(curl -s --fail https://typingmind-plugin-server.vercel.app/pad/sanity-123)
-echo "$response4"
+echo " ✦ Second GET /pad/sanity-$rand..."
+response4=$(curl -s --fail https://typingmind-plugin-server.vercel.app/pad/sanity-$rand)
+printf " ✦ → %s " "$response4"
 [[ "$response4" = "{\"text\":\"hello from curl $rand\"}" ]]
+printf "✔\n"
 
